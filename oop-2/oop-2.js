24f31c630072b5eb33264d15ca28362994f34746
MuratGider35 <m.gider35@gmail.com>
2023-09-15 20:41:46 +0300

    class declaration

---
+++ oop-2/oop-2.js
@@ -0,0 +1,40 @@
+"use strict"
+
+/* -------------------------------------------------------
+    OOP & CLASSES
+------------------------------------------------------- *
+//? OOP: Object Oriented Programming
+//? DRY: Don't Repeat Yourself
+//? BLUEPRINT: Taslak (Mimarların kullandığı mavi şablon kağıdı)
+//? CLASS: Obje türetmek için kullanılacak şablon.
+
+// Class Declaration:
+// class PascalNamedClassName { ... }
+
+// Class Expression:
+const PascalNamedClassName = class {
+
+    undefinedProperty // Onle definition ('undefined')
+    extraField = 'field-value'
+
+    //? "new Class" ile obje oluştururken veri göndermek için "constructor" methodu kullanılır.
+    constructor (parameter1, parameter2 = 'default-value') {
+        this.parameter1 = parameter1
+        this.parameter2 = parameter2
+    }
+
+    methodName1() {
+        return this
+    }
+
+    methodName2() {
+        return this.extraField
+    }
+}
+
+//? INSTANCE: Bir classtan türetilen objedir.
+const instance = new PascalNamedClassName('parameter-1-value', 'parameter-2-value')
+console.log( instance )
+console.log( instance.methodName2() )
+instance.extraField = 'new-value'
+console.log( instance.extraField )
